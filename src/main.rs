25f6b65f89524b75cd8696a200e52a484f71c725
Ameer Ibrahim <ameer@Ameers-MacBook-Pro.local>
2024-04-22 14:50:44 +0530

    feat(async): async handing of request using async-std

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
-use std::{fs, io::prelude::*, net::TcpListener, net::TcpStream, thread, time::Duration};
+use std::io::{Write, Read};
+use std::time::Duration;
+use std::fs;
+use async_std::task;
+use async_std::net::{TcpListener,TcpStream};
+use async_std::prelude::*;
+use futures::stream::StreamExt;
 
-use hello::ThreadPool;
+#[async_std::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
-
-    for stream in listener.incoming().take(2) {
-        let stream = stream.unwrap();
-        pool.execute(|| handle_connection(stream));
-    }
-    println!("shutting down!")
+    // for stream in listener.incoming() {
+    //     let stream = stream.unwrap();
+    //     handle_connection(stream).await;
+    // }
+    listener.incoming().for_each_concurrent(None,|tcpstream| async move {
+        let stream = tcpstream.unwrap();
+        handle_connection(stream).await;
+}).await;
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: impl Read + Write + Unpin) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
     let sleep = b"GET /sleep HTTP/1.1\r\n";
     let get = b"GET / HTTP/1.1\r\n";
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "hello.html")
     } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
+        task::sleep(Duration::from_secs(5)).await;
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
@@ -29,5 +36,5 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
 }
